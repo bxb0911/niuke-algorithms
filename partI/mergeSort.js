b66d0d62d902d1f5cec7ceb5cd0c9ba1bb60cc50
bxb0911 <yeonlyeo@gmail.com>
2021-05-25 09:05:51 +0800

    feat: code changes

--- partI/mergeSort.js
+++ partI/mergeSort.js
@@ -1,31 +1,47 @@
-let arr = [8, 4, 1]
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+let length
+rl.on('line', function (line) {
+  if (length) {
+    mergeSort(line, length)
+  } else {
+    length = parseInt(line)
+  }
+})
 
-function split(left, right) {
+function mergeSort(line, length) {
+  let arr = line.split(' ').map(n => parseInt(n))
+  split(0, arr.length - 1, arr)
+  console.log(arr.join(' '))
+}
+
+function split(left, right, arr) {
   if (left === right) {
     return
   }
   let middle = left + ((right - left) >> 1)
-  split(left, middle)
-  split(middle + 1, right)
-  merge(left, middle, right)
+  split(left, middle, arr)
+  split(middle + 1, right, arr)
+  merge(left, middle, right, arr)
 }
 
-function merge(left, middle, right) {
-  console.log(left, middle, right)
+function merge(left, middle, right, arr) {
   let temp = new Array(right - left + 1)
-  for (let i = left, l = left, r = middle + 1, len = temp.length; i < len; i++) {
-    if (l > middle || arr[l] > arr[r]) {
-      console.log('before-up', i, r)
-      temp[i] = arr[r++]
-      console.log('after-up', i, r)
-    } else if (r > right || arr[l] <= arr[r]) {
-      console.log('before-down', i, l)
-      temp[i] = arr[l++]
-      console.log('after-down', i, l)
-    }
+  let i = 0, l = left, r = middle + 1
+  while (l <= middle && r <= right) {
+    temp[i++] = arr[l] < arr[r] ? arr[l++] : arr[r++]
   }
-  arr = temp.slice()
+  while (l <= middle) {
+    temp[i++] = arr[l++]
+  }
+  while (r <= right) {
+    temp[i++] = arr[r++]
+  }
+  for (let i = 0, len = temp.length; i < len; i++) {
+    arr[i + left] = temp[i]
+  }
+  return arr
 }
-
-split(0, arr.length - 1)
-console.log(arr)
