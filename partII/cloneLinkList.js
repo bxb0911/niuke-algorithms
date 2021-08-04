24630494b888b26aa5d18de9e6fcb6fa8f1caa81
baiyanli <baiyanli@zuoyebang.com>
2021-08-04 20:02:46 +0800

    feat: code changes

--- partII/cloneLinkList.js
+++ partII/cloneLinkList.js
@@ -8,6 +8,9 @@ function RandomListNode(x){
 // {1,2,3,4,5,3,5,#,2,#}
 function Clone(pHead) {
   // write code here
+  while (pHead) {
+    pHead.next = { label: pHead.label, next: null, random: null };
+  }
 }
 
 module.exports = {
